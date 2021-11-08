6c5d053f1836884bee59af5c73538bed75b87657
Hampus Nilsson <hampusjojo@gmail.com>
2021-11-08 19:48:46 +0000

    modify sizes on product model and add size selector box to product template

--- products/models.py
+++ products/models.py
@@ -20,7 +20,7 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
-    has_sizes = models.BooleanField(null=True, blank=True)
+    has_sizes = models.BooleanField(null=True, blank=True, default=False)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
