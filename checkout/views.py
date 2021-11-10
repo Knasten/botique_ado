3939a81291852e947bb384a206b47a2b1174067e
Hampus Nilsson <hampusjojo@gmail.com>
2021-11-10 15:05:01 +0000

    add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JuHvAIwlcZhoWVt0Bz8CxK8kGiTNkRnaj3XqnuFLG62nGhkPC9MoNSJ4UKhIxAXFwiwoJnvpffgCxSgbEHQ8orT00gm2RQli6',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
